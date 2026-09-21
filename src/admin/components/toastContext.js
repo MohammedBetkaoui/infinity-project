5e10cc1a66b200be9a3c4f693215a4c1ebad7dde
MohammedBetkaoui <mohammed.betkaoui@univ-bba.dz>
2026-09-21 12:23:10 +0100

    edit

--- src/admin/components/toastContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from 'react'
-
-export const ToastContext = createContext({
-  success: () => {},
-  error: () => {},
-  info: () => {},
-})
-
-export const useToast = () => useContext(ToastContext)
